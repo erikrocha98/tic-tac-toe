2385c1c0c5814571978cb8dd7b9cf6e9361d3e94
erikrocha98 <146686859+erikrocha98@users.noreply.github.com>
2025-02-10 18:57:42 -0300

    feat: Adicionando reatividade ao clique nos blocos do jogo.

--- tic-tac-toe/src/app.tsx
+++ tic-tac-toe/src/app.tsx
@@ -1,23 +1,39 @@
+import styled from "styled-components";
 import Square from "./square";
+import { useState } from "react";
+
+const BoardRow = styled.div`
+    width: 100vh;
+    height: 33px;
+    display: flex;
+`
 
 export default function Board() {
+    const[squares, setSquares] = useState<Array<string>>(Array(9).fill(""));
+
+    function handleClick(){
+        const nextSquares = squares.slice();
+        nextSquares[0]="X";
+        setSquares(nextSquares);
+        console.log("quero mudar o texto para x");
+    };
     return (
         <>
-            <div className="board-row">
-                <Square />
-                <Square />
-                <Square/>
-            </div>
-            <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
-            </div>
-            <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
-            </div>
+            <BoardRow>
+                <Square handleClick={handleClick} value={squares[0]}/>
+                <Square handleClick={handleClick} value={squares[1]}/>
+                <Square handleClick={handleClick} value={squares[2]}/>
+            </BoardRow>
+            <BoardRow>
+                <Square handleClick={handleClick} value={squares[3]}/>
+                <Square handleClick={handleClick} value={squares[4]}/>
+                <Square handleClick={handleClick} value={squares[5]}/>
+            </BoardRow>
+            <BoardRow>
+                <Square handleClick={handleClick} value={squares[6]}/>
+                <Square handleClick={handleClick} value={squares[7]}/>
+                <Square handleClick={handleClick} value={squares[8]}/>
+            </BoardRow>
         </>
     )
 }
--- tic-tac-toe/src/square.tsx
+++ tic-tac-toe/src/square.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
 import styled from "styled-components";
 
 const SquareButton = styled.button`
     width:33px;
-    height:33px;
+    height:33px;    
 `;
 
-export default function Square (){
-    const[text,setText]=useState("");
+type squareProps={
+    value: string;
+    handleClick: () => void;
+}
 
-    function handleClick(){
-        console.log("quero mudar o texto para x");
-        setText("X");
-    };
+export default function Square ({value, handleClick}: squareProps){
+
     return (
-        <SquareButton onClick={handleClick}>{text}</SquareButton>
+        <SquareButton onClick={handleClick}>{value}</SquareButton>
     )
 }
